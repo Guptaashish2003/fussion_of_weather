c6e379f08b69839ad55dcf0a3ae173f59e506aa1
Ashish Gupta <gkashish1985@gmail.com>
2024-08-24 13:35:08 +0530

    update

--- app/api/test/route.js
+++ app/api/test/route.js
@@ -3,6 +3,7 @@ import { NextResponse, NextRequest } from "next/server";
 export async function GET(res, context) {
     console.log(process.env.BASE_URL,"baseusrl");
     console.log(process.env.DBURL,"dburl");
-return NextResponse.json({ success: true, message: "Hello World" }, { status: 200 });
+    const val = process.env.DBURL;
+return NextResponse.json({ success: true, message: val}, { status: 200 });
 
 }
